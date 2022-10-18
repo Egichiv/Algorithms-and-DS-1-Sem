660c72d35d83d4b504e046a3bdfb35653db529c4
Egor Ivashchenko <89981400+Egichiv@users.noreply.github.com>
2022-10-18 15:17:15 +0300

    Update task-K-ver.0.cpp

--- Lab. #2 - 2022/task-K-ver.0.cpp
+++ Lab. #2 - 2022/task-K-ver.0.cpp
@@ -4,34 +4,32 @@
 using std::cin;
 using std::cout;
 
-void k_statistic (int32_t* array, int left, int right, int position) {
-    int32_t v = array[(left + right) / 2];
+int partition(int32_t* array, int left, int right) {
+    int v = array[(left + right) / 2];
     int i = left;
     int j = right;
-
     while (i <= j) {
-
         while (array[i] < v) {
             i++;
         }
-
         while (array[j] > v) {
             j--;
         }
-
-        if (i <= j) {
-            int32_t temp = array[i];
-            array[i++] = array[j--];
-            array[j + 1] = temp;
+        if (i >= j) {
+            break;
         }
+        int32_t temp = array[i];
+        array[i++] = array[j--];
+        array[j + 1] = temp;
     }
+    return j;
+}
 
-    if ((j > left) && (position <= j)) {
-        k_statistic(array, left, j, position);
-    }
-
-    if ((i < right) && (position >= j)) {
-        k_statistic(array, j + 1, right, position);
+void quick_sort (int32_t* array, int left, int right) {
+    if (left < right) {
+        int pivot = partition (array, left, right);
+        quick_sort (array, left, pivot);
+        quick_sort (array, pivot + 1, right);
     }
 }
 
@@ -46,29 +44,18 @@ int main() {
     int32_t a1;
     int32_t a2;
 
-    cout << "\n" << "Have a nice day! Also enter the needed numbers." << "\n";
-
-    cin >> elements_number >> position;
+    cin >> elements_number >> position >> A >> B >> C >> a1 >> a2;
 
     array = (int32_t*) calloc(elements_number, sizeof(int32_t));
 
-    cin >> A >> B >> C >> a1;
-
     array[0] = a1;
-
-    if (elements_number == 1) {
-        cout << array[0];
-        return 0;
-    }
-
-    cin >> a2;
     array[1] = a2;
-
     for (int count = 2; count < elements_number; count++) {
         array[count] = (A * array[count - 2]) + (B * array[count - 1]) + C;
     }
 
-    k_statistic(array, 0, elements_number - 1, position - 1);
+    quick_sort(array, 0,elements_number - 1);
+
     cout << array[position - 1];
 
     free(array);
